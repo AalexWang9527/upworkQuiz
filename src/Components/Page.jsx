8f4fe9d2b6432f39a4fb82c2236ba924a3bcb060
RahulGautamSingh <rahultesnik@gmail.com>
2021-10-07 10:32:07 +0545

    chaned functionality

--- src/Components/Page.jsx
+++ src/Components/Page.jsx
@@ -3,62 +3,72 @@ import Option from "./Option";
 import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-function Page(props) {
+function Page() {
   let intervalId;
   let [score, setScore] = useState(0);
   let [currentIndex, setCurrentIndex] = useState(0);
   let [widthe, setWidthe] = useState(100);
+  let [viewOptions, setViewOptions] = useState(true);
+  let [msg, setMsg] = useState("");
+  let [resultIncoming, setResultIncoming] = useState(false);
   const history = useHistory();
 
   let [userData, setUserData] = useState([]);
   let userDataCopy;
-
+  let questionObj = JSON.parse(localStorage.getItem("questionObj"));
   function checkAns(index) {
+    setViewOptions(false);
+    if (widthe > 70) setMsg("You're Fast huh!");
+    else if (widthe > 30) setMsg("Okayish!!");
+    else setMsg("Toughh one!!");
     clearInterval(intervalId);
     userDataCopy = JSON.parse(JSON.stringify(userData));
 
     userDataCopy.push([
-      props.questionObj[currentIndex].question,
-      props.questionObj[currentIndex].correctOption,
+      questionObj[currentIndex].question,
+      questionObj[currentIndex].correctOption,
       index,
-      props.questionObj[currentIndex].answers[
-        props.questionObj[currentIndex].correctOption
+      questionObj[currentIndex].answers[
+        questionObj[currentIndex].correctOption
       ],
-      props.questionObj[currentIndex].answers[index],
+      questionObj[currentIndex].answers[index],
     ]);
 
     setUserData(userDataCopy);
 
-    if (currentIndex < props.questionObj.length - 1) {
-      setTimeout(() => setWidthe(100), 500);
-      setTimeout(() => setCurrentIndex(currentIndex + 1), 500);
-      if (props.questionObj[currentIndex].correctOption === index) {
+    if (currentIndex < questionObj.length - 1) {
+      setTimeout(() => setWidthe(100), 1000);
+      setTimeout(() => {
+        setCurrentIndex(currentIndex + 1);
+        setViewOptions(true);
+      }, 1000);
+      if (questionObj[currentIndex].correctOption === index) {
         setScore(score + 1);
         return "#b9f334";
       } else {
         return "#f32c1d";
       }
     } else {
-      setTimeout(() => setWidthe(100), 500);
-      if (props.questionObj[currentIndex].correctOption === index) {
+      setTimeout(() => setWidthe(100), 1000);
+      if (questionObj[currentIndex].correctOption === index) {
         setScore(score + 1);
         setTimeout(
-          () =>
+          () =>{setResultIncoming(true);
             history.push({
               pathname: "/result",
               state: { userDataCopy, score: score + 1 },
-            }),
-          500
+            })},
+          1000
         );
         return "green";
       } else {
         setTimeout(
-          () =>
+          () =>{setResultIncoming(true);
             history.push({
               pathname: "/result",
               state: { userDataCopy, score },
-            }),
-          500
+            })},
+          1000
         );
         return "#f32c1d";
       }
@@ -72,23 +82,22 @@ function Page(props) {
         userDataCopy = JSON.parse(JSON.stringify(userData));
 
         userDataCopy.push([
-          props.questionObj[currentIndex].question,
-          props.questionObj[currentIndex].correctOption,
+          questionObj[currentIndex].question,
+          questionObj[currentIndex].correctOption,
           null,
-          props.questionObj[currentIndex].answers[
-            props.questionObj[currentIndex].correctOption
+          questionObj[currentIndex].answers[
+            questionObj[currentIndex].correctOption
           ],
           "not answered",
         ]);
 
         setUserData(userDataCopy);
 
-        if (currentIndex === props.questionObj.length - 1) {
-          setTimeout(
-            () =>
-              history.push({ pathname: "/result", state: { userDataCopy } }),
-            2000
-          );
+        if (currentIndex === questionObj.length - 1) {
+          setTimeout(() => {
+            setResultIncoming(true);
+            history.push({ pathname: "/result", state: { userDataCopy } });
+          }, 2500);
         } else {
           setCurrentIndex(currentIndex + 1);
 
@@ -110,33 +119,43 @@ function Page(props) {
   return (
     currentIndex >= 0 && (
       <>
-        <div className="container">
-          <div
-            className="timer"
-            style={{
-              width: widthe + "%",
-              backgroundColor:
-                widthe > 70 ? "#b9f334" : widthe > 30 ? "orange" : "#f32c1d",
-            }}
-          ></div>
+        {resultIncoming && <h1>Let's see how you did...</h1>}
+        {!resultIncoming && (
+          <div className="container">
+            <div
+              className="timer"
+              style={{
+                width: widthe + "%",
+                backgroundColor:
+                  widthe > 70 ? "#b9f334" : widthe > 30 ? "orange" : "#f32c1d",
+              }}
+            ></div>
+            {!viewOptions && (
+              <div className="options">
+                {" "}
+                <h2>{msg}</h2>
+              </div>
+            )}
+            {viewOptions && (
+              <div className="options">
+                {questionObj[currentIndex].answers.map((choice, index) => {
+                  if (index < 4) {
+                    return (
+                      <Option
+                        value={choice}
+                        index={index}
+                        clickHandler={checkAns}
+                        key={currentIndex + "" + index}
+                      />
+                    );
+                  } else return <></>;
+                })}
+              </div>
+            )}
 
-         
-
-          <div className="options">
-            {props.questionObj[currentIndex].answers.map((choice, index) => {
-              return (
-                <Option
-                  value={choice}
-                  index={index}
-                  clickHandler={checkAns}
-                  key={currentIndex + "" + index}
-                />
-              );
-            })}
+            <Question value={questionObj[currentIndex].question} />
           </div>
-
-          <Question value={props.questionObj[currentIndex].question} />
-        </div>
+        )}
       </>
     )
   );
